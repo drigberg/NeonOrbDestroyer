756143b330d1b8cfc69b092ab0e7773474c58b2a
Daniel Rigberg <daniel.rigberg@gmail.com>
2020-11-07 19:42:33 +0100

    add basic smoke trails

--- Assets/Scripts/Meteor/MeteorMove.cs
+++ Assets/Scripts/Meteor/MeteorMove.cs
@@ -9,6 +9,8 @@ public class MeteorMove : MonoBehaviour
     public Transform wallCheckLeft;
     public Transform wallCheckRight;
     public Transform playerCheck;
+    public GameObject meteorTrailPrefab;
+    private GameObject meteorTrail;
 
     public float collisionDistance = 0.1f;
     public LayerMask groundMask;
@@ -19,38 +21,113 @@ public class MeteorMove : MonoBehaviour
     public float bounceHeight = 4f;
 
     private Vector3 velocity;
+    private enum Mode {FALLING, RUNNING};
+    private Mode mode;
+
+    private bool isGrounded;
+    private bool onWallLeft;
+    private bool onWallRight;
+    private bool onCeiling;
+    private bool hittingPlayer;
+    private bool smoking;
 
     void Start() {
+        StartFalling();
+        CreateSmokeTrail();
+    }
+
+    void StartFalling() {
+        mode = Mode.FALLING;
         velocity.x = Random.Range(maxSpeed * -0.85f, maxSpeed * 0.85f);
         velocity.y = Mathf.Sqrt(velocity.x * velocity.x + maxSpeed * maxSpeed) * -1f;
+    }
+
+    void RotateSmokeTrail() {
+        meteorTrail.transform.rotation = Quaternion.FromToRotation(Vector3.up, velocity * -1f);
+    }
+
+    void CreateSmokeTrail() {
+        // float angle = Mathf.Cos(velocity.y / velocity.x) * 180f / Mathf.PI;
+        // Quaternion rotation = Quaternion.Euler(0, 0, angle - 180f);
+        Quaternion rotation = Quaternion.FromToRotation(Vector3.up, velocity * -1f);
+        meteorTrail = Instantiate(meteorTrailPrefab, transform.position, rotation);
+        smoking = true;
+
+        // Quaternion q;
+        // vector a = crossproduct(v1, v2);
+        // q.xyz = a;
+        // q.w = sqrt((v1.Length ^ 2) * (v2.Length ^ 2)) + dotproduct(v1, v2);
     }
 
     void Update()
     {
         // check for collisions
-        bool isGrounded = Physics.CheckSphere(groundCheck.position, collisionDistance, groundMask);
-        bool onWallLeft = Physics.CheckSphere(wallCheckLeft.position, collisionDistance, groundMask);
-        bool onWallRight = Physics.CheckSphere(wallCheckRight.position, collisionDistance, groundMask);
-        bool onCeiling = Physics.CheckSphere(ceilingCheck.position, collisionDistance, groundMask);
-        bool hittingPlayer = Physics.CheckSphere(playerCheck.position, 0.6f + collisionDistance, playerMask);
+        isGrounded = Physics.CheckSphere(groundCheck.position, collisionDistance, groundMask);
+        onWallLeft = Physics.CheckSphere(wallCheckLeft.position, collisionDistance, groundMask);
+        onWallRight = Physics.CheckSphere(wallCheckRight.position, collisionDistance, groundMask);
+        onCeiling = Physics.CheckSphere(ceilingCheck.position, collisionDistance, groundMask);
+        hittingPlayer = Physics.CheckSphere(playerCheck.position, 0.6f + collisionDistance, playerMask);
 
         // explode if hitting player or ground
         if (hittingPlayer) {
             Explode();
-        } else if (isGrounded) {
-            Explode();
         }
 
-        // bounce off walls
+        if (mode == Mode.FALLING) {
+            Fall();
+        } else {
+            Run();
+        }
+
+    }
+
+    float GetXDirection() {
+        return velocity.x > 0f ? 1f : -1f;
+    }
+
+    void BounceOffWalls() {
         bool hittingWall = (onWallLeft && velocity.x < 0) || (onWallRight && velocity.x > 0);
         if (hittingWall) {
             velocity.x *= -1f;
+        }
+        if (smoking) {
+            RotateSmokeTrail();
+        }
+    }
+    void Fall() {
+        BounceOffWalls();
+        meteorTrail.transform.position = transform.position;
+
+        if (isGrounded) {
+            mode = Mode.RUNNING;
+            velocity.x = maxSpeed * GetXDirection();
+            velocity.y = 0f;
+            StopSmoking();
         }
 
         transform.Translate(velocity * Time.deltaTime);
     }
 
+    void Run() {
+        BounceOffWalls();
+        if (isGrounded) {
+            velocity.y = 0f;
+        } else {
+            velocity.x = Mathf.Sqrt(maxSpeed * maxSpeed / 2f) * GetXDirection();
+            velocity.y = Mathf.Sqrt(maxSpeed * maxSpeed / 2f) * -1f;
+        }
+        transform.Translate(velocity * Time.deltaTime);
+    }
+
     void Explode() {
+        StopSmoking();
         Destroy(gameObject);
+    }
+
+    void StopSmoking() {
+        if (smoking) {
+            Destroy(meteorTrail);
+            smoking = false;
+        }
     }
 }
