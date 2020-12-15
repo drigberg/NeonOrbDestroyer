c387e63f60f3da2b9c63a3bbd118c308f2a43ae2
Daniel Rigberg <daniel.rigberg@gmail.com>
2020-12-14 22:46:14 -0600

    begin NPCs

---
+++ Assets/Scripts/NPCs/NPC.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+    public string[] messages;
+    public Collider trigger;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnTriggerEnter(Collider collider) {
+        if (collider.tag == "Player") {
+            Debug.Log("ENTER!");
+        }
+    }
+
+    void OnTriggerExit(Collider collider) {
+        if (collider.tag == "Player") {
+            Debug.Log("EXIT!");
+        }
+    }
+}
